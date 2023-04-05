f16dfe91d049712ea732b9437ea60f8b1c9e752a
GoodBlock-Nill <nill@good-block.com>
2023-04-05 17:40:00 +0900

    update

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#yourPrimaryColor',
+    },
+    secondary: {
+      main: '#yourSecondaryColor',
+    },
+    // Add more custom colors or configurations
+  },
+  // Add more customizations like typography, breakpoints, etc.
+});
+
+export default theme;
